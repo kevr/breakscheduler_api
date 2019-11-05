7f12282735cf5d1c49b141f16742d6267fe73a83
Kevin Morris <kmorris@zerocost.dev>
2019-11-05 08:10:05 -0800

    introduce User model
    Generated via devise and intended to be used
    for JWT-authenticated API sessions.
    
    We now have two models for users:
        User
            Basic users who are meant to login via API
        AdminUser
            Administrators who are given access to /admin
            (active_admin routes)
    
    Additionally, add the User model to active_admin and
    remove the Comments section. Rename active_admin's
    comment model to avoid possible future conflicts.
    
    Signed-off-by: Kevin Morris <kmorris@zerocost.dev>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # Standard users
+  # We provide our own rolled routes for user
+  # endpoints; users should only be communicating
+  # with rails via json api communication
+  # devise_for :users
+
+  # Administrators who have access to /admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
