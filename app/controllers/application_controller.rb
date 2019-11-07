826ae0312fe7de147371ae753b732cc1f5868aab
Kevin Morris <kmorris@zerocost.dev>
2019-11-07 05:00:28 -0800

    Allow any host and method in CORS
    Signed-off-by: Kevin Morris <kmorris@zerocost.dev>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :setup_cors
+
+  def setup_cors
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 
   def json_authenticated
     _json_authenticated
