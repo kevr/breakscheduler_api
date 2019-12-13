ba4c2a4d035c1246456dd3105d9b319d3ca2c861
Kevin Morris <kmorris@zerocost.dev>
2019-12-12 20:24:36 -0800

    Add name field to AdminUsers form
    Signed-off-by: Kevin Morris <kmorris@zerocost.dev>

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,16 +1,27 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :name, :email, :password, :password_confirmation
+
+  controller do
+    def update
+      if params[:admin_user][:password].blank? && params[:admin_user][:password_confirmation].blank?
+        params[:admin_user].delete("password")
+        params[:admin_user].delete("password_confirmation")
+      end
+      super
+    end
+  end
 
   index do
     selectable_column
     id_column
+    column :name
     column :email
     column :current_sign_in_at
-    column :sign_in_count
     column :created_at
     actions
   end
 
+  filter :name
   filter :email
   filter :current_sign_in_at
   filter :sign_in_count
@@ -18,6 +29,7 @@ ActiveAdmin.register AdminUser do
 
   form do |f|
     f.inputs do
+      f.input :name
       f.input :email
       f.input :password
       f.input :password_confirmation
