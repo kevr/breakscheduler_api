3c3f9372eb61ec67707b2d2b7d7bf46d454ba612
Kevin Morris <kevr.gtalk@gmail.com>
2020-02-21 00:52:20 -0800

    Use default generated comments, just uncomment what we need
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Article do
-  permit_params :subject, :body, :order
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :subject, :body, :order
+  #
+  # or
+  #
+  permit_params do
+    permitted = [:subject, :body, :order]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  end
+  
 end
